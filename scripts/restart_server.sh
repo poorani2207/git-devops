1d94accb93654485415614bc0c094350e672a3bf
poorani2207 <105795661+poorani2207@users.noreply.github.com>
2025-11-28 13:13:39 +0530

    Update restart_server.sh

--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 
-echo "Restarting Apache on Amazon Linux..."
-systemctl restart httpd
-echo "Apache restarted."
+echo "Restarting Apache on Amazon Linux 2023..."
 
-
+if command -v httpd >/dev/null 2>&1; then
+    sudo systemctl restart httpd
+    exit 0
+else
+    echo "Apache (httpd) is NOT installed!"
+    exit 1
+fi
